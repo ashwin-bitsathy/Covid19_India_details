fc2499a75352a4b71c714e39ea82a5c558fa099a
ashwin-bitsathy <58909026+ashwin-bitsathy@users.noreply.github.com>
2020-03-23 14:33:56 +0530

    Update corono_nodb.py

--- corono_nodb.py
+++ corono_nodb.py
@@ -1,6 +1,6 @@
 import requests
 import lxml.html as lh
-
+ 
 def y(ip):
  
     try:
@@ -15,25 +15,48 @@ def y(ip):
         col =[]
         
         flag = 0
-        id =0
+        boundary =0
+        h = "0123456789"
+
         for t in tr_elements:
                     if(flag==0):
-                        flag=1
+                        flag+=1
                         continue
+                    
+                        
+                        
                     name=t.text_content()
                     op = str(name)
                     op = " ".join(op.split())
-                    id+=1
+                    if(op=="DATE TITLE"):
+                        continue
+                    sid = ""
+                    for i in op:
+                        if  i in h:
+                            sid+=i
+                        else:
+                            break
+
+                    for i in op:
+                        if(i=='-' or i=='/'):
+                            boundary =1
+                    if boundary:
+                        boundary=0
+                        continue
+
+
                     x = {
                        "State":op,
-                       "_id":id
+                       "_id":sid
                     }
-                    col.append(x)
                     
-        
+                    
+                    col.append(x)
+
+
         
         for it in col:
-            if(it['_id']==ip+5):
+            if(it['_id']==ip):
                 print("state: "+it['State'][2:-7]+'\n')
                 print("Total Confirmed cases (Indian National) :"+it['State'][-7])
                 print("Total Confirmed cases ( Foreign National ) :"+it['State'][-5])
@@ -45,28 +68,30 @@ def y(ip):
         print("Code must be debugged")
    
 
-print("1    Andhra Pradesh")
-print("2    Chhattisgarh")
-print("3    Delhi")
-print("4    Gujarat")
-print("5    Haryana")
-print("6    Himachal Pradesh")
-print("7    Karnataka")
-print("8    Kerala")
-print("9    Madhya Pradesh")
-print("10   Maharashtra")        
-print("11   Odisha")
-print("12   Puducherry  ")
-print("13  Punjab")  
-print("14  Rajasthan")   
-print("15  Tamil Nadu ") 
-print("16  Telengana")  
-print("17  Chandigarh")  
-print("18  Jammu and Kashmir ") 
-print("20  Uttar Pradesh ") 
-print("21  Uttarakhand") 
-print("22  West Bengal \n")
-i = int(input("Enter the State Id: "))
+print('1 Andhra Pradesh')
+print('2 Bihar ')
+print('3 Chhattisgarh')
+print('4 Delhi') 
+print('5 Gujarat')
+print('6 Haryana') 
+print('7 Himachal Pradesh') 
+print('8 Karnataka')
+print('9 Kerala') 
+print('10 Madhya Pradesh')
+print('11 Maharashtra') 
+print('12 Odisha') 
+print('13 Puducherry') 
+print('14 Punjab')
+print('15 Rajasthan') 
+print('16 Tamil Nadu')
+print('17 Telengana')
+print('18 Chandigarh') 
+print('19 Jammu and Kashmir') 
+print('20 Ladakh' )
+print('21 Uttar Pradesh' )
+print('22 Uttarakhand') 
+print('23 West Bengal')
+i = input("Enter the State Id: ")
 y(i)
 
 
